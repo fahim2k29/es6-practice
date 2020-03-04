46566a6a5dac1581a020172bab0d382165584dd9
Fahim <55667286+fahim2k29@users.noreply.github.com>
2020-03-04 22:44:47 +0600

    Clear

---
+++ null-vs-undefine.js
@@ -0,0 +1,18 @@
+let Pakhi;
+
+function add(num1,num2){
+    console.log(num1+num2);
+    return;
+}
+
+function add(num1, num2){
+console.log(num1, num2);
+}
+const result = add(13);
+
+const premik = {name: "smart Dude", phone:24365};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
